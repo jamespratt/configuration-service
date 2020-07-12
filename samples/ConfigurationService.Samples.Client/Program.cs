961ee948340b012ee2fbd368594f0e895e75fa45
James Pratt <jamesdpratt@gmail.com>
2020-07-12 15:54:17 -0500

    Read config form local appsettings

--- samples/ConfigurationService.Samples.Client/Program.cs
+++ samples/ConfigurationService.Samples.Client/Program.cs
@@ -24,9 +24,9 @@ namespace ConfigurationService.Samples.Client
                 .AddConfiguration(configuration)
                 .AddRemoteSource(new RemoteConfigurationSource
                 {
-                    ConfigurationName = "test.json",
-                    ConfigurationServiceUri = "http://localhost:5000/configuration/",
-                    SubscriberConfiguration = "localhost:6379",
+                    ConfigurationName = configuration["ConfigurationName"],
+                    ConfigurationServiceUri = configuration["ConfigurationServiceUri"],
+                    SubscriberConfiguration = configuration["SubscriberConfiguration"],
                     Optional = false,
                     ReloadOnChange = true,
                     LoggerFactory = loggerFactory
