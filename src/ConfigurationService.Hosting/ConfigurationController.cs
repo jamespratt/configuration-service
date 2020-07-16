cbf917ed3ee9549c89d55bf22c20af4aeb39b01b
James Pratt <jamesdpratt@gmail.com>
2020-07-16 17:59:16 -0500

    Change controller list endpoint

--- src/ConfigurationService.Hosting/ConfigurationController.cs
+++ src/ConfigurationService.Hosting/ConfigurationController.cs
@@ -1,5 +1,4 @@
 ﻿using System.Collections.Generic;
-using System.Linq;
 using System.Net;
 using System.Text;
 using Microsoft.AspNetCore.Mvc;
@@ -16,6 +15,22 @@ namespace ConfigurationService.Hosting
         public ConfigurationController(IProvider provider)
         {
             _provider = provider;
+        }
+
+        /// <summary>
+        /// Returns a list of all configuration file names.
+        /// </summary>
+        /// <response code="200">Returns a list of all configuration file names</response>
+        /// <response code="400">Returns details specifying why the request is not valid</response> 
+        [HttpGet()]
+        [Produces("application/json", Type = typeof(List<string>))]
+        [ProducesResponseType(typeof(IEnumerable<string>), 200)]
+        [ProducesResponseType(typeof(ProblemDetails), 400)]
+        public IActionResult List()
+        {
+            var files = _provider.ListAllFiles();
+
+            return Ok(files);
         }
 
         /// <summary>
@@ -40,22 +55,6 @@ namespace ConfigurationService.Hosting
             var fileContent = Encoding.UTF8.GetString(bytes);
 
             return Content(fileContent);
-        }
-
-        /// <summary>
-        /// Returns a list of all configuration file names.
-        /// </summary>
-        /// <response code="200">Returns a list of all configuration file names</response>
-        /// <response code="400">Returns details specifying why the request is not valid</response> 
-        [HttpGet(nameof(List))]
-        [Produces("application/json", Type = typeof(List<string>))]
-        [ProducesResponseType(typeof(IEnumerable<string>), 200)]
-        [ProducesResponseType(typeof(ProblemDetails), 400)]
-        public IActionResult List()
-        {
-            var files = _provider.ListAllFiles().OrderBy(o => o);
-
-            return Ok(files);
         }
     }
 }
