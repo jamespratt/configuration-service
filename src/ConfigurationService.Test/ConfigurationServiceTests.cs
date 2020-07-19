1219c5ba4a98aa4e58ba61a396538d46b427d080
James Pratt <jamesdpratt@gmail.com>
2020-07-19 17:38:38 -0500

    Add unit tests

---
+++ src/ConfigurationService.Test/ConfigurationServiceTests.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+using ConfigurationService.Hosting;
+using ConfigurationService.Hosting.Providers;
+using ConfigurationService.Hosting.Publishers;
+using Microsoft.Extensions.Logging;
+using NSubstitute;
+using Xunit;
+
+namespace ConfigurationService.Test
+{
+    public class ConfigurationServiceTests
+    {
+        private readonly ILogger<Hosting.ConfigurationService> _logger;
+        private readonly IProvider _provider;
+        private readonly IPublisher _publisher;
+        private readonly IConfigurationService _configurationService;
+
+
+        public ConfigurationServiceTests()
+        {
+            _logger = Substitute.For<ILogger<Hosting.ConfigurationService>>();
+            _publisher = Substitute.For<IPublisher>();
+            _provider = SetupStorageProvider();
+            _configurationService = new Hosting.ConfigurationService(_logger, _provider, _publisher);
+        }
+
+        [Fact]
+        public async Task Publish_Invoked_on_Initialization()
+        {
+            await _configurationService.InitializeProvider();
+
+            await _publisher.Received().Publish(Arg.Any<string>(), Arg.Any<string>());
+        }
+
+        [Fact]
+        public async Task Publish_Invoked_on_Change()
+        {
+            await _configurationService.OnChange(ListRandomFiles(1));
+
+            await _publisher.Received(1).Publish(Arg.Any<string>(), Arg.Any<string>());
+        }
+
+        [Fact]
+        public async Task Publish_Invoked_on_PublishChanges()
+        {
+            await _configurationService.PublishChanges(ListRandomFiles(1));
+
+            await _publisher.Received(1).Publish(Arg.Any<string>(), Arg.Any<string>());
+        }
+
+        [Fact]
+        public async Task Publish_Invoked_on_Change_for_Each_File()
+        {
+            var fileCount = 5;
+            await _configurationService.OnChange(ListRandomFiles(fileCount));
+
+            await _publisher.Received(fileCount).Publish(Arg.Any<string>(), Arg.Any<string>());
+        }
+
+        [Fact]
+        public async Task Publish_Is_Not_Invoked_when_No_Change()
+        {
+            await _configurationService.OnChange(new List<string>());
+
+            await _publisher.DidNotReceive().Publish(Arg.Any<string>(), Arg.Any<string>());
+        }
+
+        [Fact]
+        public async Task Service_Permits_Null_Publisher()
+        {
+            var configurationService = new Hosting.ConfigurationService(_logger, _provider);
+            await configurationService.PublishChanges(ListRandomFiles(1));
+        }
+
+        [Fact]
+        public async Task Publish_Is_Not_Invoked_when_No_Publisher_Registered()
+        {
+            var configurationService = new Hosting.ConfigurationService(_logger, _provider);
+            await configurationService.OnChange(ListRandomFiles(1));
+
+            await _publisher.DidNotReceive().Publish(Arg.Any<string>(), Arg.Any<string>());
+        }
+
+        private IProvider SetupStorageProvider()
+        {
+            var storageProvider = Substitute.For<IProvider>();
+            storageProvider.ListAllFiles().Returns(ListRandomFiles(1));
+            storageProvider.GetFile(Arg.Any<string>()).Returns(name => Encoding.UTF8.GetBytes($"{{ \"name\": \"{name}\" }}"));
+            return storageProvider;
+        }
+
+        private static IEnumerable<string> ListRandomFiles(int count)
+        {
+            var list = new List<string>();
+
+            for (int i = 0; i < count; i++)
+            {
+                list.Add($"{Guid.NewGuid()}.json");
+            }
+
+            return list;
+        }
+    }
+}
